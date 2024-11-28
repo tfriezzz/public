40380cf9c422a8ddbf87499eba074ec9e79fe906
tfriezz <roswall.tim@gmail.com>
2024-11-28 16:08:24 +0900

    update TestTextNodeToHTMLNode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -22,12 +22,29 @@ class TestTextNode(unittest.TestCase):
 
 
 
-class TestTextNodeToHtmlNode(unittest.TestCase):
-    def test_text_conversion(self):
-        node = TextNode("Hello", TextType.TEXT)
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
         html_node = text_node_to_html_node(node)
-        self.assertIsNone(html_node.tag)
-        self.assertEqual(html_node.value, "Hello")
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "https://www.image.org")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.image.org", "alt": "This is an image"},
+        )
+
+    def test_bold(self):
+        node = TextNode("This is bold", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold")
+
 
 if __name__ == "__main__":
     unittest.main()
